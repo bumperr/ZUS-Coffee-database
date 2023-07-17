bbb00abe4e5f24ef8f0561f52b9393389105d415
Ali Azeem <87369905+bumperr@users.noreply.github.com>
2023-07-17 11:23:45 +0800

    Update file4.sql
    finalised

--- file4.sql
+++ file4.sql
@@ -1,36 +1,33 @@
---File 4 , data and attribute need to adjust to get 2 row
 SELECT 
-    E.Employee_Name,
-    V.Vec_plateNum,
+    E.E_position,
+    Ps.Per_firstname,
+    D.Dpt_name,
+    V.Vec_price,
     I.Itm_name,
-    A.Application_Date,
-    B.Branch_Location,
-    C.Customer_Name,
-    P.Procurement_Date,
-    S.Shipment_Date
-FROM
-    Employee E
-JOIN
-    Employee_Vehicle EV ON E.E_ID = EV.E_ID
-JOIN
-    Vehicle V ON EV.Vec_ID = V.Vec_ID
-JOIN
-    Item I ON V.Vec_ID = I.Vec_ID
-JOIN
-    Application A ON A.B_ID = V.B_ID
+    B.B_name,
+    P.Proc_totalCost,
+    S.Ship_receiveDate
+FROM Employee E
+
+JOIN Person Ps ON E.Per_email=Ps.Per_email
+
+JOIN Employee_Vehicle EV ON E.E_ID = EV.E_ID
+
+JOIN Vehicle V ON EV.Vec_ID = V.Vec_ID
+
+JOIN Item I ON V.Vec_ID = I.Vec_ID
+
+JOIN Department D ON D.Dpt_ID= E.Dpt_ID
+
 JOIN
     Branch B ON V.B_ID = B.B_ID
-JOIN
-    Customer C ON C.Customer_ID = I.Customer_ID
 JOIN
     Procurement P ON P.Proc_ID = I.Proc_ID
 JOIN
     Shipment S ON S.Ship_ID = I.Ship_ID
 WHERE
-    A.Application_Points > 100
-    AND V.Vec_price > 50000
-    AND I.Itm_cost < 100
-    AND E.Employee_Position = 'Manager'
-ORDER BY
-    E.Employee_ID DESC, V.Vec_ID DESC, I.Itm_ID DESC, A.Application_ID DESC
-FETCH FIRST 2 ROWS ONLY;
+    S.ship_status='Completed'
+    AND V.Vec_price > 3500
+    AND I.Itm_cost < 300
+    AND E.E_employmentStatus = 'Permanent'
+ORDER BY V.Vec_price DESC;
